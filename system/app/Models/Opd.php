8de8332e6a0da0b25408ea356981a525d06e4cb4
Junaidi <junaidiju4109@gmail.com>
2023-07-29 00:06:24 +0700

    push lagi

--- system/app/Models/Opd.php
+++ system/app/Models/Opd.php
@@ -1,10 +1,25 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\ModelAuthenticate;
+use Illuminate\Support\Str;
 
-use App\Models\Model;
-
-class Opd extends Model
+class Opd extends ModelAuthenticate
 {
     protected $table = "opd";
+
+    function handleUploadLogo()
+    {
+        if (request()->hasFile('logo')) {
+            $logo = request()->file('logo');
+            $destination = "opd";
+            $randomStr = Str::random(5);
+            $filename = time() . "-"  . $randomStr . "."  . $logo->extension();
+            $url = $logo->storeAs($destination, $filename);
+            $this->logo = "app/" . $url;
+            $this->save();
+
+        }
+    }
 }
