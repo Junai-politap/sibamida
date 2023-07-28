c0d5b425f4a8aec6451ea21750912467dacf0211
Muhammad Luqinul Mifdlol Assiddiqi <101262825+Mluqnll@users.noreply.github.com>
2023-07-28 21:46:03 +0700

    gass

---
+++ system/resources/views/opd/pegawai/create.blade.php
@@ -0,0 +1,106 @@
+<x-opd>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- left column -->
+                <div class="col-md-12">
+                    <!-- general form elements -->
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Quick Example</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <!-- form start -->
+                        <form>
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="exampleInputText">Nama</label>
+                                    <input type="text" class="form-control"
+                                        placeholder="Masukkan Nama Pegawai">
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="exampleInputText">Username</label>
+                                            <input type="text" class="form-control"
+                                                placeholder="Masukkan Username Pegawai">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="exampleInputText">Jabatan</label>
+                                            <input type="text" class="form-control"
+                                                placeholder="Masukkan Jabatan Pegawai">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="exampleInputText">NIK</label>
+                                            <input type="text" class="form-control"
+                                                placeholder="Masukkan NIK Pegawai">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="exampleInputText">NIP</label>
+                                            <input type="text" class="form-control"
+                                                placeholder="Masukkan NIP Pegawai">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="exampleInputText">Alamat</label>
+                                            <input type="text" class="form-control"
+                                                placeholder="Masukkan Alamat Pegawai">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="exampleInputText">Nomor Handphone</label>
+                                            <input type="text" class="form-control"
+                                                placeholder="Masukkan Nomor Handphone Pegawai">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="exampleInputText">Foto</label>
+                                            <input type="file" class="form-control"
+                                                placeholder="Masukkan Foto Pegawai">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="exampleInputPassword">Password</label>
+                                            <input type="password" class="form-control"
+                                                placeholder="Masukkan Password Pegawai">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.card-body -->
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary float-right">Simpan</button>
+                    </div>
+                    </form>
+                </div>
+                <!-- /.card -->
+
+                <!-- /.card -->
+
+            </div>
+        </div>
+        </div>
+    </section>
+</x-opd>
