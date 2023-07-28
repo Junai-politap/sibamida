10cd9b9e764d8260a5ec1327e641ee73322f4cca
Junaidi <junaidiju4109@gmail.com>
2023-07-28 21:33:34 +0700

    update Model

--- system/app/Models/Aset.php
+++ system/app/Models/Aset.php
@@ -2,10 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Model;
 
 class Aset extends Model
 {
-    use HasFactory;
+    protected $table ="aset";
 }
